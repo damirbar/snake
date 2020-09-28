6119d6429add32833da924c1e73148069f05272f
DESKTOP-D5S5QEJ\damir <damirbar@gmail.com>
2020-09-28 11:16:39 +0300

    - Player can replay after losing - High score is saved and displayed

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import random
 
+import os
 import pygame
 
 
 # Constants
-SNAKE_BLOCK_SIZE = 10
-SCREEN_HEIGHT    = 600
-SCREEN_WIDTH     = 800
+SNAKE_BLOCK_SIZE      = 10
+SCREEN_HEIGHT         = 600
+SCREEN_WIDTH          = 800
+LOSE_QUIT             = 1
+LOSE_REPLAY           = 2
+FONT_SIZE             = 25
+SNAKE_HIGH_SCORE_PATH = './.snake_high_score'
 
 # Colors
 class Colors:
@@ -19,7 +24,7 @@ class Colors:
 
 class Location:
 
-    def __init__(self, x, y):
+    def __init__(self, x=0, y=0):
         self.__x, self.__y = x, y
 
     @property
@@ -62,18 +67,13 @@ class Snake:
 
     @head_loc.setter
     def head_loc(self, loc):
-        # num_links = len(self.__links)
-        # for i in range(num_links - 1):
-            # self.__links[num_links - i - 2] = self.__links[num_links - i - 1]
-            # self.__links[i] = self.__links[i]
         if not self.ate_food:
             self.__links.pop()
         else:
             self.ate_food = False
         self.__links.insert(0, loc)
 
-    def add_link(self, loc):
-        # self.__links.append(loc)
+    def add_link(self):
         self.ate_food = True
 
     @property
@@ -93,12 +93,34 @@ class SnakeGame:
         self.snake_speed          = snake_speed
         self.food_location        = self.generate_random_location()
         self.keep_playing         = True
-        self.snake_is_dead        = False
+        self.is_snake_dead        = False
         self.direction            = None
         self.is_direction_changed = False
         self.location_modify      = None
-        self.score_font = pygame.font.SysFont("calibri", 25)
+        self.score_font           = pygame.font.SysFont("calibri", FONT_SIZE)
+        self.font_style           = pygame.font.SysFont("bahnschrift", FONT_SIZE)
+        self.high_score           = self.read_high_score()
 
+    def read_high_score(self):
+        if os.path.exists(SNAKE_HIGH_SCORE_PATH):
+            with open(SNAKE_HIGH_SCORE_PATH) as score_file:
+                try:
+                    return int(score_file.read())
+                except:
+                    return 0
+
+    def update_high_score(self):
+        with open(SNAKE_HIGH_SCORE_PATH, 'w') as score_file:
+            score_file.write(str(self.high_score))
+
+    def restart(self):
+        self.snake = Snake(Location(int(SCREEN_WIDTH / 2), int(SCREEN_HEIGHT / 2)))
+        self.food_location = self.generate_random_location()
+        self.keep_playing = True
+        self.is_snake_dead = False
+        self.direction = None
+        self.is_direction_changed = False
+        self.location_modify = None
 
     def init_display(self):
         pygame.init()
@@ -115,21 +137,31 @@ class SnakeGame:
 
     @staticmethod
     def is_out_of_bounds(loc):
-        if loc.x < 0 or loc.y < 0 or loc.x > SCREEN_WIDTH or loc.y > SCREEN_HEIGHT:
+        if loc.x < 0 or loc.y < 0 or loc.x > SCREEN_WIDTH-1 or loc.y > SCREEN_HEIGHT-1:
             return True
-
-    @staticmethod
-    def handle_snake_is_dead():
-        exit(0)
 
     def score_counter(self):
         value = self.score_font.render(f"Score: {len(self.snake.links)}", True, Colors.white)
         self.disp.blit(value, [10,10])
 
-    def handle_event(self, evt):
+        if self.high_score is not None:
+            value = self.score_font.render(f"High Score: {self.high_score}", True, Colors.white)
+            self.disp.blit(value, [10, 37])
+
+    def should_continue_playing(self, evt):
+        if evt.type == pygame.KEYDOWN:
+            if evt.key == pygame.K_c:
+                return LOSE_REPLAY
+            else:
+                return LOSE_QUIT
+        if evt.type == pygame.QUIT:
+            return LOSE_QUIT
+
+    def handle_play_event(self, evt):
         # If the user attempts to close the game window
         if evt.type == pygame.QUIT:
             self.keep_playing = False
+            exit(0)
 
         # If the user pressed a key
         if evt.type == pygame.KEYDOWN and not self.is_direction_changed:
@@ -150,18 +182,24 @@ class SnakeGame:
                 self.is_direction_changed = True
                 self.location_modify = Location(0, SNAKE_BLOCK_SIZE)
 
-    def game_loop(self):
-        self.location_modify = Location(0, 0)
+    def prompt(self, msg, color):
+        lines = msg.splitlines()
+        for i, line in enumerate(lines):
+            prompt_line = self.font_style.render(line, True, color)
+            self.disp.blit(prompt_line, [SCREEN_WIDTH/6, SCREEN_HEIGHT/3 + (FONT_SIZE*i + 2)])
 
-        while self.keep_playing and not self.snake_is_dead:
+    def game_loop(self):
+        self.location_modify = Location()
+
+        while self.keep_playing and not self.is_snake_dead:
             self.is_direction_changed = False
 
             for evt in pygame.event.get():
-                self.handle_event(evt)
+                self.handle_play_event(evt)
 
 
             if self.snake.head_loc == self.food_location:
-                self.snake.add_link(self.snake.head_loc)
+                self.snake.add_link()
                 self.food_location = self.generate_random_location()
 
             # Update the snake's location
@@ -169,7 +207,11 @@ class SnakeGame:
 
             if self.is_out_of_bounds(self.snake.head_loc) or self.snake.head_loc in self.snake.links[1:]:
                 print(f"Score: {len(self.snake.links)}")
-                self.handle_snake_is_dead()
+                self.is_snake_dead = True
+
+                if self.high_score is None or len(self.snake.links) > self.high_score:
+                    self.high_score = len(self.snake.links)
+                    self.update_high_score()
             else:
                 self.disp.fill(Colors.black)
                 pygame.draw.rect(self.disp, Colors.red,
@@ -185,13 +227,34 @@ class SnakeGame:
                 # Refresh rate according to the selected snake speed
                 self.clock.tick(self.snake_speed)
 
-        pygame.quit()
-        quit()
 
+
+def play_game(game):
+    while True:
+        game.game_loop()
+
+        if game.is_snake_dead:
+            game.prompt("Play another round?\nC - continue\nAny other key to quit", Colors.red)
+            game.score_counter()
+            pygame.display.update()
+
+            user_res = None
+
+            while user_res is None:
+                for evt in pygame.event.get():
+                    user_res = game.should_continue_playing(evt)
+
+            if user_res == LOSE_REPLAY:
+                game.restart()
+            else:
+                pygame.quit()
+                exit(0)
 
 def main():
     game = SnakeGame()
-    game.game_loop()
+    play_game(game)
+
+
 
 if __name__ == '__main__':
     main()
